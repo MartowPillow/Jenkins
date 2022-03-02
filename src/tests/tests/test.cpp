3532e477e51d9eb5a636cb2d7a7a7d69bc3c1319
Thomas Yriarte <thomas.yriarte@etu.isima.fr>
2022-03-02 23:49:50 +0100

    'fix' error

--- src/tests/tests/test.cpp
+++ src/tests/tests/test.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 0);
+TEST(TestCaseName, Good) {
+  EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, Bad) {
+  EXPECT_EQ(1, 0);
 }
